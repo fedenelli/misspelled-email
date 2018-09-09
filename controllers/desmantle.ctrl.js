0dd07b08578b92db7f4e68d5f6be49e55b365523
Federico Nelli <fn.nelli@gmail.com>
2018-09-09 03:08:31 -0300

    Removed Validator dependency

--- controllers/desmantle.ctrl.js
+++ controllers/desmantle.ctrl.js
@@ -1,10 +1,8 @@
 "use strict";
 
-const validator = require('validator');
-
 const desmantle = (email) => {
     return new Promise((resolve, reject) => {
-        if (validator.isEmail(email)) {
+        if (email.indexOf('@') != -1 && email.indexOf('.') != -1) {
             email = email.split("@");
             resolve({ success: true, data: { user: email[0], domain: email[1] } });
         } else {
